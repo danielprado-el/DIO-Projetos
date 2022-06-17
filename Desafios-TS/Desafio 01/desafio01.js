6973a8a3f8b99f174623d8ccdb1fc3df958705ee
danielprado-el <danprado17.dp@gmail.com>
2022-06-17 14:35:43 -0300

    Atualizando repositorio

--- Desafios-TS/Desafio 01/desafio01.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const employee = {
-    code: 10,
-    name: "John",
-};
-console.log(employee.code);
-console.log(employee.name);
--- Desafios-TS/Desafio 01/desafio01.ts
+++
@@ -1,7 +0,0 @@
-const employee: {code: number, name: String} = {
-  code: 10,
-  name: "John",
-}
-
-console.log(employee.code)
-console.log(employee.name)
